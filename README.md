4ca93af1fa88feb0aef8f4da9bbaa5d1e415aa4c
bclauson <clauson.branden@gmail.com>
2021-11-20 22:11:23 -0800

    pass test input 1, epect Beep!

---
+++ README.md
@@ -0,0 +1,7 @@
+**Describe: numberAnalyze();**
+<br>
+Test: If we input 1, we want to output "Beep!"
+<br>
+Code: numberAnalyze("1");
+<br>
+Expect: "Beep!"
---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Business Logic
+
+function numberAnalyze(number) {
+
+  const result = [];
+
+  if (number.includes("1")) {
+
+    result.push("Beep!");
+
+  }
+
+  return result;
+
+}
+
